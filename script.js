1225ccf13bac11f9deb94a99b1fb605c06fea669
Elise <elise.celadon@edu.univ-eiffel.fr>
2026-02-25 16:04:14 +0100

    udapte script

--- script.js
+++ script.js
@@ -1,30 +1,16 @@
 
 
-document.addEventListener("DOMContentLoaded", function() {
-   const btn = document.getElementById("lang-btn");
-   const titre = document.getElementById("titre");
-   const metier = document.getElementById("metier");
-   const description = document.getElementById("description");
-   let en = false;
 
-   // Texte par défaut en français
-   titre.textContent = "Bienvenue sur mon Portfolio 2026";
-   metier.textContent = "Créatice & Graphiste";
-   description.textContent = "Voici une sélection de mes projets récents et de mes réalisations professionnelles.";
 
-   btn.addEventListener("click", function() {
-      if (!en) {
-         titre.textContent = "Welcome to my Portfolio 2026";
-         metier.textContent = "Creator & Graphic Designer";
-         description.textContent = "Here is a selection of my recent projects and professional achievements.";
-         btn.textContent = "FR";
-         en = true;
-      } else {
-         titre.textContent = "Bienvenue sur mon Portfolio 2026";
-         metier.textContent = "Créatice & Graphiste";
-         description.textContent = "Voici une sélection de mes projets récents et de mes réalisations professionnelles.";
-         btn.textContent = "EN";
-         en = false;
-      }
-   });
+
+fetch('en.json').then(function(response) { 
+   response.json().then(function(langue){ 
+      console.log(langue);
+      Object.keys(langue).forEach(function(cle) {
+    document.querySelector("#" + cle).innerHTML = langue[cle];
 });
+      // Ajoutez ici d’autres instructions utilisant la variable data 
+   }) 
+})
+
+//bouton passage fr en 
